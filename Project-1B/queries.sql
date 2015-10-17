5a39e3f7ab6ae1258dc299618238501283d1eced
Sandra Suttiratana <suttiras@ucla.edu>
2015-10-17 16:15:36 -0700

    Added "Die Another Day" to be Table Name

--- Project-1B/queries.sql
+++ Project-1B/queries.sql
@@ -1,3 +1,3 @@
-SELECT CONCAT(Actor.first, ' ', Actor.last) 
+SELECT CONCAT(Actor.first, ' ', Actor.last) as 'Actors in Die Another Day'
 FROM Movie, MovieActor, Actor 
 WHERE Movie.id = MovieActor.mid AND MovieActor.aid = Actor.id AND Movie.title = 'Die Another Day';
