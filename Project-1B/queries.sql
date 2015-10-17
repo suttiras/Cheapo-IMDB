217c747a96383c3223ca3a547e2e8b2b46e24eb2
Sandra Suttiratana <suttiras@ucla.edu>
2015-10-17 16:24:55 -0700

    Added a comment

--- Project-1B/queries.sql
+++ Project-1B/queries.sql
@@ -1,3 +1,7 @@
 SELECT CONCAT(Actor.first, ' ', Actor.last) as 'Actors in Die Another Day'
 FROM Movie, MovieActor, Actor 
 WHERE Movie.id = MovieActor.mid AND MovieActor.aid = Actor.id AND Movie.title = 'Die Another Day';
+
+--Get the first names and last names of the actors whose ID matches the actor ID listed in 
+--MovieActor and the movie ID listed in MovieActor matches the movie ID listed in Movie that is 
+--paired with "Die Another Day"
