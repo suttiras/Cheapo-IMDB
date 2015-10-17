98d178f17c7cdc5dbd6d0524b5a358f4a8878a7a
Sandra Suttiratana <suttiras@ucla.edu>
2015-10-17 16:07:52 -0700

    Added new lines to make it more readable

--- Project-1B/queries.sql
+++ Project-1B/queries.sql
@@ -1,1 +1,3 @@
-SELECT Actor.first, Actor.last FROM Movie, MovieActor, Actor WHERE Movie.id = MovieActor.mid AND MovieActor.aid = Actor.id AND Movie.title = 'Die Another Day';
+SELECT Actor.first, Actor.last 
+FROM Movie, MovieActor, Actor 
+WHERE Movie.id = MovieActor.mid AND MovieActor.aid = Actor.id AND Movie.title = 'Die Another Day';
