c2a9f52c4a3b9c81e0d93954159aa9aecdf05d84
Sandra Suttiratana <suttiras@ucla.edu>
2015-10-17 16:12:12 -0700

    Added Concat

--- Project-1B/queries.sql
+++ Project-1B/queries.sql
@@ -1,3 +1,3 @@
-SELECT Actor.first, Actor.last 
+SELECT CONCAT(Actor.first, ' ', Actor.last) 
 FROM Movie, MovieActor, Actor 
 WHERE Movie.id = MovieActor.mid AND MovieActor.aid = Actor.id AND Movie.title = 'Die Another Day';
