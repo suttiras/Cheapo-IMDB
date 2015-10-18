426100e4560090bdf35b9f4e1d62629328aaa3c1
Sandra Suttiratana <suttiras@ucla.edu>
2015-10-17 18:23:04 -0700

    Added constraints to Review Table and made a test for Review's constraints

---
+++ Project-1B/testConstraint.sql
@@ -0,0 +1,2 @@
+INSERT INTO Review VALUES("Richard Roeper", "2015-10-18T01:04:25+00:00",1000,2,"This movie stinks!");
+INSERT INTO Review VALUES("Roger Ebert", "2015-10-18T01:04:25+00:00",1000,0,"This movie is an atrocity!");
