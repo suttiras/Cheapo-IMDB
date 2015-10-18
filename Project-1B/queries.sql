8679d1c4d47627558ab04700b5e27a81b513ce06
srsuttiratana <sarah_suttiratana@hotmail.com>
2015-10-17 18:03:31 -0700

    Added Brad Pitt Query

--- Project-1B/queries.sql
+++ Project-1B/queries.sql
@@ -28,3 +28,10 @@ HAVING COUNT(*) >= 2) T;
 --and then count the number of movies that that actor acted in (with the number of movies 
 --being represented in each row). Then count the total number of actors who have acted in 
 --multiple movies by counting the number of rows.
+
+SELECT Movie.title AS 'Movies That Brad Pitt Has Acted In'
+FROM MovieActor, Actor, Movie
+WHERE Actor.first = 'Brad' AND Actor.last = 'Pitt' AND MovieActor.aid = Actor.id AND MovieActor.mid = Movie.id;
+
+--Match the actor's first name and last name to "Brad Pitt" and then made sure that the MovieActor's aid
+--matched Actor's id. Also made sure that the MovieActor's movie id matched the Movie's id.
