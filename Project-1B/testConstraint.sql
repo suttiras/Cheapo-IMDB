59737ad38ad020ad4a2bba6045c4b801de55abd7
Sandra Suttiratana <suttiras@ucla.edu>
2015-10-17 18:30:14 -0700

    Added a new test

--- Project-1B/testConstraint.sql
+++ Project-1B/testConstraint.sql
@@ -1,2 +1,3 @@
 INSERT INTO Review VALUES("Richard Roeper", "2015-10-18T01:04:25+00:00",1000,2,"This movie stinks!");
 INSERT INTO Review VALUES("Roger Ebert", "2015-10-18T01:04:25+00:00",1000,0,"This movie is an atrocity!");
+INSERT INTO Review VALUES("Gourdy", "2015-10-18T01:04:25+00:00",1000,100,"I think this movie is GREAT! 100 stars!!!");
