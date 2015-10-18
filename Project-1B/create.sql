285b024f3d0650d09c1e0b760e57a20ed28e56d1
Sandra Suttiratana <suttiras@ucla.edu>
2015-10-17 19:09:24 -0700

    Added Foreign Key Constraints
    For MovieGenre and Review

--- Project-1B/create.sql
+++ Project-1B/create.sql
@@ -12,7 +12,9 @@ CHECK (sex = 'Female' OR sex = 'Male')
 CREATE TABLE Director(id INT PRIMARY KEY, last VARCHAR(20) NOT NULL, first VARCHAR(20) NOT NULL, dob DATE NOT NULL, 
 dod DATE)ENGINE = INNODB;
 
-CREATE TABLE MovieGenre(mid INT, genre VARCHAR(20) NOT NULL, CHECK (mid > 0))ENGINE = INNODB;
+CREATE TABLE MovieGenre(mid INT, genre VARCHAR(20) NOT NULL, CHECK (mid > 0),
+FOREIGN KEY (mid) references Movie(id)
+)ENGINE = INNODB;
 
 CREATE TABLE MovieDirector(mid INT, did INT, CHECK (mid > 0), CHECK (did > 0),
 FOREIGN KEY (did) references Director(id),
@@ -28,7 +30,8 @@ FOREIGN KEY (mid) references Movie(id)
 
 CREATE TABLE Review(name VARCHAR(20) NOT NULL, time TIMESTAMP, mid INT NOT NULL, rating INT NOT NULL, comment VARCHAR(500),
 CHECK (rating BETWEEN 1 and 5),
-CHECK (mid > 0)
+CHECK (mid > 0),
+FOREIGN KEY (mid) references Movie(id)
 )ENGINE = INNODB;
 
 CREATE TABLE MaxPersonID(id INT)ENGINE = INNODB;
