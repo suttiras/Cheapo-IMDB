63b6b9f80ede5f66bc5ae7dbc92708898cb2e3bc
Sandra Suttiratana <suttiras@ucla.edu>
2015-10-17 23:33:07 -0700

    Added a different violation test

--- Project-1B/testViolations.sql
+++ Project-1B/testViolations.sql
@@ -1,2 +1,3 @@
-INSERT INTO Movie VALUES(10000, "Aladdin", 1992, "G","Walt Disney Studios");
-INSERT INTO Movie VALUES(20000, "Kung Fu Panda", 2008, "G","DreamWorks Studios");
+UPDATE Movie 
+SET id=300000
+WHERE id=10000;
