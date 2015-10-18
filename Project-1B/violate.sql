fa545d14e05fd99451c918779c0a01e963d2e79f
Sandra Suttiratana <suttiras@ucla.edu>
2015-10-17 21:21:22 -0700

    Added more constraint violations for Movie, Actor

--- Project-1B/violate.sql
+++ Project-1B/violate.sql
@@ -1,4 +1,28 @@
+--Let's pretend we start with a brand new database
+INSERT INTO Movie VALUES(2, "Aladdin", 1992, "G","Walt Disney Studios");
+INSERT INTO Movie VALUES(2, "The Simpsons Movie", 2007, "PG","20th Century Fox");
+--violates the PRIMARY KEY constraint since ID's for both movies are the same.
 
+INSERT INTO Movie VALUES(3, "The Simpsons Movie", -69, "PG","20th Century Fox");
+--violates the CHECK constraint since the year is less than 1.
+
+INSERT INTO Movie VALUES(-3, "The Simpsons Movie", -69, "PG","20th Century Fox");
+--violates the CHECK constraint since the ID is less than 0.
+
+INSERT INTO Actor VALUES(-3, "Black", "Jack", "Male",19690828,\N);
+--violates the CHECK constraint since the ID is less than 0.
+
+INSERT INTO Actor VALUES(3, "Black", "Jack", "Alien",19690828,\N);
+--violates the CHECK constraint since the sex is not female or male.
+
+--Let's pretend we start with a brand new database
+INSERT INTO Actor VALUES(3, "Black", "Jack", "Male",19690828,\N);
+INSERT INTO Actor VALUES(3, "Black", "Jack", "Male",19690828,\N);
+
+(id INT PRIMARY KEY, last VARCHAR(20) NOT NULL, first VARCHAR(20) NOT NULL, dob DATE NOT NULL, 
+dod DATE)
+
+INSERT INTO Director VALUES(
 
 INSERT INTO Review VALUES("Roger Ebert", "2015-10-18T01:04:25+00:00",1000,0,"This movie is an atrocity!");
 --violates the Review CHECK constraint of having a rating between 1 and 5.
