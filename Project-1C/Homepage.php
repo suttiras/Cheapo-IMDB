8997b985ab570ced06c1d1c42eb54f6a026bd01e
Sandra Suttiratana <suttiras@ucla.edu>
2015-10-23 19:18:07 -0700

    Added a homepage and logo

---
+++ Project-1C/Homepage.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+	<title>IMDB: Cheapo Version</title>
+</head>
+<body>
+<p><img src="ImdbLogo.png" alt="Website Logo"></p>
+<h1><font size = "5"><b>Programmed by the Twins</b></font></h1>
+<h2><font size = "3"><b>For those who are too lazy to visit Imdb.com (or somehow don't know that it exists)</b></font></h2>
+</body>
+</html>
