4d7c991ad525d200fcadd8547e48baf2dab66a4e
Sandra Suttiratana <suttiras@ucla.edu>
2015-10-23 19:35:07 -0700

    Add new file

---
+++ Project-1C/director_to_movie.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+	<title>IMDB: Cheapo Version</title>
+</head>
+<body>
+<p style="text-align:center"><img src="ImdbLogo.png" alt="Website Logo"></p>
+<p></p>
+<p>
+Add Director to Movie Page
+<a href="./add_person.php">Actor/Director</a>
+</p>
