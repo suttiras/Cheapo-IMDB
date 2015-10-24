a49402b584dfb0e0ea38ed04206cd1568cf471f3
Sandra Suttiratana <suttiras@ucla.edu>
2015-10-23 20:07:08 -0700

    Added menus, etc.

---
+++ Project-1C/add_comments.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+	<title>IMDB: Cheapo Version</title>
+</head>
+<body>
+<p style="text-align:center"><a href="./Homepage.php"><img src="ImdbLogo.png" alt="Website Logo"></a></p>
+
+<p>
+Add Comments Page
+</p>
+
+
+<h2 style="text-align:center"><font size = "3"><b>Add to Database</b></font></h2>
+<p style="text-align:center">
+<a href="./add_person.php">Actor/Director</a>
+</p>
+<p style="text-align:center">
+<a href="./add_movie.php">Movie</a>
+</p>
+<p style="text-align:center">
+<a href="./add_comments.php">Reviews</a>
+</p>
+<p style="text-align:center">
+<a href="./director_to_movie.php">Add an Actor to a Movie</a>
+</p>
+<p style="text-align:center">
+<a href="./director_to_movie.php">Add a Director to a Movie</a>
+</p>
+<p></p>
+<h2 style="text-align:center"><font size = "3"><b>Browse Database</b></font></h2>
+<p style="text-align:center">
+<a href="./browse_actor.php">Actor</a>
+</p>
+
+<p style="text-align:center">
+<a href="./browse_movie.php">Movie</a>
+</p>
+
+</body>
+</html>
