fc211f61fceccaf00af130b5720ff75d48699adc
Sandra Suttiratana <suttiras@ucla.edu>
2015-10-23 19:29:38 -0700

    Made a new php file

--- Project-1C/Homepage.php
+++ Project-1C/Homepage.php
@@ -6,5 +6,9 @@
 <p style="text-align:center"><img src="ImdbLogo.png" alt="Website Logo"></p>
 <!--<h1 style="text-align:center"><font size = "5"><b>Programmed by the Twins</b></font></h1>-->
 <h2 style="text-align:center"><font size = "3"><b>For those who are too lazy to visit Imdb.com (or somehow don't know that it exists)</b></font></h2>
+<p>
+<a href="./">Actor/Director</a>
+</p>
+
 </body>
 </html>
