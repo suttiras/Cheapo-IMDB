4bbb49a9f199de29f7ed48834028570224e6424e
Sandra Suttiratana <suttiras@ucla.edu>
2015-10-23 19:21:53 -0700

    Centered font

--- Project-1C/Homepage.php
+++ Project-1C/Homepage.php
@@ -3,8 +3,8 @@
 	<title>IMDB: Cheapo Version</title>
 </head>
 <body>
-<p><img src="ImdbLogo.png" alt="Website Logo"></p>
-<h1><font size = "5"><b>Programmed by the Twins</b></font></h1>
-<h2><font size = "3"><b>For those who are too lazy to visit Imdb.com (or somehow don't know that it exists)</b></font></h2>
+<p style="text-align:center"><img src="ImdbLogo.png" alt="Website Logo"></p>
+<h1 style="text-align:center"><font size = "5"><b>Programmed by the Twins</b></font></h1>
+<h2 style="text-align:center"><font size = "3"><b>For those who are too lazy to visit Imdb.com (or somehow don't know that it exists)</b></font></h2>
 </body>
 </html>
