125994786a010b0459933cc65bf55d67974b2134
Sandra Suttiratana <suttiras@ucla.edu>
2015-10-25 21:05:50 -0700

    Added year, rating, company, and genre

--- Project-1C/browse_movie.php
+++ Project-1C/browse_movie.php
@@ -27,13 +27,18 @@ else if ($_GET["id"] != "")
 
 	//list actors/roles
 	
-	$sql="SELECT title FROM Movie WHERE id =" . $id; 
+	$sql="SELECT title, year, rating, company FROM Movie WHERE id =" . $id; 
 	$pdo_obj = get_pdo();
 	
 	foreach ($pdo_obj->query($sql) as $row){//Array or records stored in $row
-	$formatted_name = $row[title];}
+	$formatted_name = $row[title];
+	$formatted_year = $row[year];
+	$formatted_rating = $row[rating];
+	$formatted_company = $row[company];
+	}
 	echo "<h1 style='text-align:center'>";
 	echo $formatted_name;
+	echo " (" . $formatted_year . ")";
 	echo "</h1>";
 	echo "<h3> Actors </h3>";
 	$actor_query = "SELECT CONCAT(Actor.first, ' ', Actor.last) as 'Actor1', MovieActor.role as role, Actor.id as id
@@ -63,8 +68,27 @@ else if ($_GET["id"] != "")
 	echo $formatted_name;
 	echo "<br>";
 	}
-	echo "<br>";
 	//end of list director
+	
+	//get genre, rating, company
+	echo "<br>";
+	echo "<b>Rated: </b>" . $formatted_rating . "<br>";
+	echo "<b>Production Company: </b>" . $formatted_company . "<br>";
+	
+	$genre_query = "SELECT genre
+	FROM MovieGenre
+	WHERE MovieGenre.mid = " . $id;
+	echo "<br><b> Genre: </b><br>";
+	foreach ($pdo_obj->query($genre_query) as $row){//Array or records stored in $row
+	$formatted_genre = $row[genre];
+	echo $formatted_genre;
+	echo "<br>";
+	}
+	
+	//end of genre, rating, company
+	
+	echo "<br>";
+	
 	/*
 	Review(name VARCHAR(20) NOT NULL, time TIMESTAMP, mid INT NOT NULL, rating INT NOT NULL, comment
 	*/
