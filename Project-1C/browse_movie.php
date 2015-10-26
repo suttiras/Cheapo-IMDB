12f4943ec03b5f8b559e20f2c7f6d785eb2b541c
Sandra Suttiratana <suttiras@ucla.edu>
2015-10-25 19:24:48 -0700

    Got the Reviews to work

--- Project-1C/browse_movie.php
+++ Project-1C/browse_movie.php
@@ -55,7 +55,41 @@ if ($_GET["id"] != "")
 	}
 	echo "<br>";
 	//end of list director
+	/*
+	Review(name VARCHAR(20) NOT NULL, time TIMESTAMP, mid INT NOT NULL, rating INT NOT NULL, comment
+	*/
 	
+	//list reviews
+	$review_query_2 = "SELECT AVG(Review.rating) as averageRtg FROM Review, Movie
+	WHERE Movie.id = Review.mid AND Movie.id =" . $id;
+	
+	$review_query = "SELECT Review.name as name, Review.time as time, Review.rating as rating, Review.comment as comment
+	FROM Review, Movie
+	WHERE Movie.id = Review.mid AND Movie.id =" . $id;
+	
+	echo "<br><h2> Reviews </h2>";
+	
+	foreach ($pdo_obj->query($review_query_2) as $row){//Array or records stored in $row
+	$average_rating = $row[averageRtg];
+	//echo $formatted_name;
+	$review_average = "<b> AVERAGE RATING: " . $average_rating . "</b><br>";
+	echo $review_average;
+	echo "<br>";
+	}
+	
+	foreach ($pdo_obj->query($review_query) as $row){//Array or records stored in $row
+	$formatted_name = $row[name];
+	//echo $formatted_name;
+	$review_title = "<b>" . $formatted_name . "</b>" . " rates this movie: <b>" . $row[rating] . " star(s) </b><br>";
+	echo $review_title;
+	echo $row[time];
+	echo "<br>";
+	echo $row[comment];
+	echo "<br>";
+	echo "<br>";
+	}
+	echo "<br>";
+	//end of list reviews
 }
 
 
