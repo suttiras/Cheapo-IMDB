7ba3e08d2ed095ca183132be674667013a63c05a
Sandra Suttiratana <suttiras@ucla.edu>
2015-10-25 19:01:34 -0700

    Printed the Actors and Directors

--- Project-1C/browse_movie.php
+++ Project-1C/browse_movie.php
@@ -5,9 +5,65 @@
 <body>
 <p style="text-align:center"><a href="./Homepage.php"><img src="ImdbLogo.png" alt="Website Logo"></a></p>
 
+<h2>
+Movie Information for:
+</h2>
+<!--Search-->
 <p>
-Browse Movie Page
-</p>
+<form action="" method="GET">
+<?php
+
+if ($_GET["id"] != "")
+{
+	require_once('global_functions.php');
+	$id = $_GET["id"];
+
+	//list actors/roles
+	
+	$sql="SELECT title FROM Movie WHERE id =" . $id; 
+	$pdo_obj = get_pdo();
+	
+	foreach ($pdo_obj->query($sql) as $row){//Array or records stored in $row
+	$formatted_name = $row[title];}
+	echo "<h2>";
+	echo $formatted_name;
+	echo "<h2>";
+	echo "<h3> Actors </h3>";
+	$actor_query = "SELECT CONCAT(Actor.first, ' ', Actor.last) as 'Actor1', MovieActor.role as role
+	FROM Movie, MovieActor, Actor 
+	WHERE Movie.id = MovieActor.mid AND MovieActor.aid = Actor.id AND Movie.id =" . $id;
+	foreach ($pdo_obj->query($actor_query) as $row){//Array or records stored in $row
+	$formatted_name = $row[Actor1];
+	$role = $row[role];
+	echo "<b>";
+	echo $formatted_name;
+	echo "</b>";
+	echo " as ";
+	echo $role;
+	echo "<br>";
+	}
+	//end of list actors/roles
+	//list director
+	$director_query = "SELECT CONCAT(Director.first, ' ', Director.last) as 'Director1'
+	FROM Movie, MovieDirector, Director
+	WHERE Movie.id = MovieDirector.mid AND MovieDirector.did = Director.id AND Movie.id =" . $id;
+	echo "<br><b> Directed by: </b><br>";
+	foreach ($pdo_obj->query($director_query) as $row){//Array or records stored in $row
+	$formatted_name = $row[Director1];
+	echo $formatted_name;
+	echo "<br>";
+	}
+	echo "<br>";
+	//end of list director
+	
+}
+
+
+
+?>
+</form>
+<!--End Search-->
+
 
 <p style="text-align:center">
 <a href="./search.php">Search</a>
