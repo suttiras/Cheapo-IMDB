8b686a7d4c04abd4382ef375d485a4721fd1ffc7
srsuttiratana <sarah_suttiratana@hotmail.com>
2015-11-11 17:57:20 -0800

    Modified read and write for BTLeafNode

--- Project-2/bruinbase/BTreeNode.cc
+++ Project-2/bruinbase/BTreeNode.cc
@@ -19,7 +19,11 @@ BTLeafNode::BTLeafNode()
  * @return 0 if successful. Return an error code if there is an error.
  */
 RC BTLeafNode::read(PageId pid, const PageFile& pf)
-{ return 0; }
+{ 
+	memset(buffer, 0, PageFile::PAGE_SIZE);
+	RC rc = pf.read(pid, buffer);
+	return rc;
+}
     
 /*
  * Write the content of the node to the page pid in the PageFile pf.
@@ -28,7 +32,10 @@ RC BTLeafNode::read(PageId pid, const PageFile& pf)
  * @return 0 if successful. Return an error code if there is an error.
  */
 RC BTLeafNode::write(PageId pid, PageFile& pf)
-{ return 0; }
+{ 
+	RC rc = pf.wrie(pid, buffer);
+	return rc; 
+}
 
 /*
  * Return the number of keys stored in the node.
