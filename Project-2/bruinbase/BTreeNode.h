f95f1ccbe51651f98d2776024b6bbef61c4a075e
Sandra Suttiratana <suttiras@ucla.edu>
2015-11-11 17:24:47 -0800

    Updated header file with new functions and added BTreeNonLeafNode class

--- Project-2/bruinbase/BTreeNode.h
+++ Project-2/bruinbase/BTreeNode.h
@@ -18,6 +18,8 @@
  */
 class BTLeafNode {
   public:
+  
+	BTLeafNode();
    /**
     * Insert the (key, rid) pair to the node.
     * Remember that all keys inside a B+tree node should be kept sorted.
@@ -97,12 +99,15 @@ class BTLeafNode {
     * @return 0 if successful. Return an error code if there is an error.
     */
     RC write(PageId pid, PageFile& pf);
+	
+	void print();	//for testing
 
   private:
    /**
     * The main memory buffer for loading the content of the disk page 
     * that contains the node.
     */
+	int numOfKeys;
     char buffer[PageFile::PAGE_SIZE];
 }; 
 
@@ -183,5 +188,84 @@ class BTNonLeafNode {
     */
     char buffer[PageFile::PAGE_SIZE];
 }; 
+
+/*
+ * BTNonLeafNode: The class representing a B+tree nonleaf node.
+ */
+class BTNonLeafNode {
+  public:
+	BTNonLeafNode();
+   /**
+    * Read the content of the node from the page pid in the PageFile pf.
+    * @param pid[IN] the PageId to read
+    * @param pf[IN] PageFile to read from
+    * @return 0 if successful. Return an error code if there is an error.
+    */
+    RC read(PageId pid, const PageFile& pf);
+
+   /**
+    * Write the content of the node to the page pid in the PageFile pf.
+    * @param [IN] the PageId to write to
+    * @param [IN] PageFile to write to
+    * @return 0 if successful. Return an error code if there is an error.
+    */
+    RC write(PageId pid, PageFile& pf);
+
+   /**
+    * Insert the (key, pid) pair to the node.
+    * Remember that all keys inside a B+tree node should be kept sorted.
+    * @param [IN] the key to insert
+    * @param [IN] the PageId to insert
+    * @return 0 if successful. Return an error code if the node is full.
+    */
+    RC insert(int key, PageId pid);
+
+   /**
+    * Insert the (key, pid) pair to the node
+    * and split the node half and half with sibling.
+    * The middle key after the split is returned in midKey.
+    * Remember that all keys inside a B+tree node should be kept sorted.
+    * @param key[IN] the key to insert
+    * @param pid[IN] the PageId to insert
+    * @param sibling[IN] the sibling node to split with. This node MUST be empty when this function is called.
+    * @param midKey[OUT] the key in the middle after the split. This key should be inserted the parent node.
+    * @return 0 if successful. Return an error code if there is an error.
+    */
+    RC insertAndSplit(int key, PageId pid, BTNonLeafNode& sibling, int& midKey);
+
+   /**
+    * Given the searchKey, find the child-node pointer to follow and
+    * output it in pid.
+    * @param searchKey[IN] the searchKey that is being looked up.
+    * @param pid[OUT] the pointer to the child node to follow.
+    * @return 0 if successful. Return an error code if there is an error.
+    */
+    RC locateChildPtr(int searchKey, PageId& pid);
+
+   /**
+    * Initialize the root node with (pid1, key, pid2).
+    * @param pid1[IN] the first PageId to insert
+    * @param key[IN] the key that should be inserted between the two PageIds
+    * @param pid2[IN] the PageId to insert behind the key
+    * @return 0 if successful. Return an error code if there is an error.
+    */
+    RC initializeRoot(PageId pid1, int key, PageId pid2);
+
+   /**
+    * Return the number of keys stored in the node.
+    * @return the number of keys in the node
+    */
+    int getKeyCount();
+	
+	void print();	//for testing
+
+ private:
+   /**
+    * The main memory buffer for loading the content of the disk page 
+    * that contains the node.
+    */
+	int numOfKeys;
+    char buffer[PageFile::PAGE_SIZE];
+};
 
 #endif /* BTREENODE_H */
